3c4b5da7a47f6b197d40c66b750825599273b826
Zeropoet <mancel@zeropoet.xyz>
2026-02-18 16:19:35 -0500

    Render static grid squares without media frames

--- main.js
+++ main.js
@@ -1,16 +1,10 @@
 (function () {
   var grid = document.getElementById("grid");
-  var modules = window.MODULES || [];
+  var squareCount = 9;
 
-  modules.forEach(function (module, index) {
+  Array.from({ length: squareCount }).forEach(function () {
     var tile = document.createElement("div");
     tile.className = "tile";
-
-    var frame = document.createElement("iframe");
-    frame.loading = "lazy";
-    frame.src = "./module-frame.html?i=" + index;
-    frame.title = module.name || "module-" + (index + 1);
-    tile.appendChild(frame);
     grid.appendChild(tile);
   });
 })();
